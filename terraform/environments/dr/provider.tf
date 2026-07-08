1597efb8592b1b71df0e2a7e82842f9c84153e8b
Sagar Uprety <sagarupreti100@gmail.com>
2026-07-08 19:05:22 +0200

    feat(terraform): apply required resource tags

--- terraform/environments/dr/provider.tf
+++ terraform/environments/dr/provider.tf
@@ -10,4 +10,11 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
+  default_tags {
+    tags = {
+      Project     = "sentinel-aws-dr"
+      ManagedBy   = "terraform"
+      Environment = "dr"
+    }
+  }
 }
